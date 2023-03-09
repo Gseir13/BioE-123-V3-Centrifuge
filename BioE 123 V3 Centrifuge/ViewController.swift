20bfb669b6168f1406cb6920c02c259483043021
Gabe and Sam <arizeanalytics@gmail.com>
2023-03-09 14:28:56 -0800

    Initial Commit

---
+++ BioE 123 V3 Centrifuge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BioE 123 V3 Centrifuge
+//
+//  Created by Gabriel Seir on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
