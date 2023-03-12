548dfad4dd663612f0c1327f9bbaea441843220f
Gabe and Sam <arizeanalytics@gmail.com>
2023-03-11 18:19:50 -0800

    RunningViewController

---
+++ BioE 123 V3 Centrifuge/RunningViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RunningViewController.swift
+//  BioE 123 V3 Centrifuge
+//
+//  Created by Gabriel Seir on 3/11/23.
+//
+
+import UIKit
+
+class RunningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
